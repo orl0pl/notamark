4dc342694f4a214447539cd3ae47092ffe3fba9c
orl0pl <orlyyt@int.pl>
2023-05-26 16:26:53 +0200

    update

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+const diff = require('diff');
 var cookieParser = require('cookie-parser')
 const app: Express = express();
 const iconmap = require('./utils/iconmap.json');
-const fs = require('fs');
+import fs from 'fs';
 var loggedInSessions: {
   [key: string]: number
 };
@@ -11,31 +12,41 @@ interface Account {
   name: string;
   password: string;
   roles: Array<'editor' | 'admin' | 'user'>;
+  id: number;
+}
+interface Changeset {
+  id: number;
+  summary: string;
+  old: Changeset['id'];
+  new: string;
+  madeBy: Account["id"],
+  madeAt: number;
+  changes: [
+    {
+      count: number,
+      added: boolean | undefined,
+      removed: boolean | undefined,
+      value: string
+    }
+  ];
+  verified: boolean;
 }
 var accounts: Account[];
+var changesets: Changeset[];
 accounts = JSON.parse(fs.readFileSync('db/accounts.json', 'utf8'));
+changesets = JSON.parse(fs.readFileSync('db/changesets.json', 'utf8'));
 loggedInSessions = JSON.parse(fs.readFileSync('db/loggedInSessions.json', 'utf8'));
+declare global {
+  namespace Express {
+    interface Request {
+      account: Account | null;
+    }
+  }
+}
 function saveToDB(): void {
   fs.writeFileSync('db/accounts.json', JSON.stringify(accounts, null, 2));
   fs.writeFileSync('db/loggedInSessions.json', JSON.stringify(loggedInSessions, null, 2));
 }
-// = [
-//   {
-//     name: 'test1',
-//     password: 'test1',
-//     roles: ['editor']
-//   },
-//   {
-//     name: 'admin',
-//     password: 'admin',
-//     roles: ['admin']
-//   },
-//   {
-//     name: 'test2',
-//     password: 'test2',
-//     roles: ['user']
-//   }
-// ];
 function logger(req: Request , res: Response, next: Function) {
   console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
   next();
@@ -46,32 +57,45 @@ function iconmapper(name: string){
     return String.fromCodePoint(parseInt(codepoint, 16))
 }
 function userAuthData(req: Request, res: Response, next: Function){
-  if(loggedInSessions[req.cookies.sID]){
-
+  if(accounts[loggedInSessions[req.cookies.sID]]){
+    req.account = accounts[loggedInSessions[req.cookies.sID]]
   }
+  else {
+    req.account = null;
+  }
+  next()
 }
+
+const string1 = "Hi!";
+const string2 = "Hello world!";
+
+const changes = diff.diffWordsWithSpace(string1, string2);
+
+console.log(changes);
+
 app.use('/static',express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(logger)
+app.use(userAuthData)
 app.set('view engine', 'ejs')
 app.get('/editor', (req, res) => {
-    res.render('editor', {
-        url: '../',
-        mi: iconmapper,
-    })
+    if(req.account?.roles.includes('editor')){
+        res.render('editor', {
+            url: '../../',
+            mi: iconmapper,
+            error: null,
+            account: req.account
+        })
+
+    }
+    else {
+        res.send('You cannot edit')
+    }
 })
 app.get('/', (req: Request, res: Response) => {
   var response = ``
-  if(req.cookies.sID){
-    const currentAccount = accounts[loggedInSessions[req.cookies.sID]]
-    response = `You are logged in as ${currentAccount.name}
-    you can ${currentAccount.roles.join(' or ')}
-    `
-  }
-  else {
-    response = `You are not logged in`
-  }
+  req.account ? response = `You are logged in as ${req.account.name}` : response = `You are not logged in`
   res.send(response)
 });
 app.get('/user', (req: Request, res: Response) => {
@@ -118,7 +142,8 @@ app.post('/user/login', (req: Request, res: Response) => {
     res.render('login', {
       url: '../../',
       mi: iconmapper,
-      error: 403
+      error: 403,
+      
     })
   }
 })
