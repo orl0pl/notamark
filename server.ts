25a4fb71fd75556369b3764abb91e938eeb13ca1
orl0pl <orlyyt@int.pl>
2023-06-06 22:09:12 +0200

    update

--- server.ts
+++ server.ts
@@ -191,6 +191,48 @@ app.get('/s/:id/l/:lessonid/n/:noteid', (req: Request<{ id: number, lessonid: nu
     res.redirect('back')
   }
 })
+app.get('/s/:id/l/:lessonid/e/:exerciseid', (req: Request<{ id: number, lessonid: number, exerciseid: number }>, res: Response) => {
+  const subject = data.subjects[req.params.id]
+  const lesson = subject.lessons[req.params.lessonid]
+  const exercise = lesson.exercises[req.params.exerciseid]
+  var converter = new showdown.Converter()
+  converter.setOption('simpleLineBreaks', true);
+
+  if (lesson && subject && exercise) {
+
+    var renderedHtml: string = converter.makeHtml(exercise.solution || '');
+    var html = sanitizeHtml(renderedHtml);
+    html = html.replace(/\$\$(.*?)\$\$/g, function (outer: any, inner: string) {
+      return katex.renderToString(inner, { displayMode: true, throwOnError: false, errorColor: 'var(--md-sys-color-error)' });
+    }).replace(/\\\[(.*?)\\\]/g, function (outer: any, inner: any) {
+      return katex.renderToString(inner, { displayMode: true, throwOnError: false, errorColor: 'var(--md-sys-color-error)' });
+    }).replace(/\\\((.*?)\\\)/g, function (outer: any, inner: any) {
+      return katex.renderToString(inner, { displayMode: false, throwOnError: false, errorColor: 'var(--md-sys-color-error)' });
+    }).replace(/\$(.*?)\$/g, function (outer: any, inner: string) {
+      return katex.renderToString(inner, { displayMode: false, throwOnError: false, errorColor: 'var(--md-sys-color-error)' });
+    })
+    res.render('exercise', {
+      account: req.account,
+      url: '../../../../../../',
+      mi: iconmapper,
+      timeAgo: timeAgo,
+      subjects: data.subjects,
+      subject: subject,
+      lessons: subject.lessons,
+      persons: data.persons,
+      lesson: lesson,
+      selectedSubjectId: req.params.id,
+      selectedLessonId: req.params.lessonid,
+      selectedexerciseId: req.params.exerciseid,
+      rawContent: lesson.exercises[req.params.exerciseid].solution || '',
+      selectedLesson: lesson,
+      renderedContent: html
+    })
+  }
+  else {
+    res.redirect('back')
+  }
+})
 app.get('/user', (req: Request, res: Response) => {
   var response = ``
   console.log(loggedInSessions)
