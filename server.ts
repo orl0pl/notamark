5f5d8434df98b743f651e556b84945c1a7fe1dd0
orl0pl <orlyyt@int.pl>
2023-06-11 10:43:32 +0200

    enought for today

--- server.ts
+++ server.ts
@@ -1,16 +1,30 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { Convert, DataBase } from "./db/converter";
+import { Convert } from "./db/converter";
 import fs from 'fs';
-import TimeAgo from 'javascript-time-ago'
-import pl from 'javascript-time-ago/locale/pl'
+import TimeAgo from 'javascript-time-ago';
+import pl from 'javascript-time-ago/locale/pl';
 TimeAgo.addDefaultLocale(pl)
 export const timeAgo = new TimeAgo('pl')
+import { Account, SessionsArray } from './interfaces';
+import { setupReactViews } from "express-tsx-views";
+import { Props } from "./tsx-views/my-view";
+// ROUTES
+import editorRoute from './routes/editorRoute';
+import indexRoute from './routes/indexRoute';
+import subjectRoute from './routes/subjectRoute';
+import lessonRoute from './routes/lessonRoute';
+import noteRoute from './routes/noteRoute';
+import { addRoute, addPOSTRoute } from './routes/addRoutes';
+import exerciseRoute from './routes/exerciseRoute';
+import userRoute from './routes/userRoute';
+import { loginRoute, loginPOSTRoute } from './routes/loginRoutes';
+import { logoutRoute, logoutPOSTRoute } from './routes/logoutRoutes';
+import {editRoute, editPOSTRoute} from './routes/editRoutes';
 var json = fs.readFileSync('db/notes.json', 'utf8');
 export const data = Convert.toDataBase(json);
 var cookieParser = require('cookie-parser')
 const app: Express = express();
-import { Account, Changeset, SessionsArray } from './interfaces';
 export var loggedInSessions: SessionsArray;
 export var accounts: Account[];
 accounts = JSON.parse(fs.readFileSync('db/accounts.json', 'utf8'));
@@ -22,7 +36,6 @@ declare global {
     }
   }
 }
-import editorRoute from './routes/editorRoute';
 export function saveToDB(): void {
   fs.writeFileSync('db/accounts.json', JSON.stringify(accounts, null, 2));
   fs.writeFileSync('db/loggedInSessions.json', JSON.stringify(loggedInSessions, null, 2));
@@ -36,25 +49,22 @@ function userAuthData(req: Request, res: Response, next: Function) {
   }
   next()
 }
-import { setupReactViews } from "express-tsx-views";
-import { Props } from "./tsx-views/my-view";
-import indexRoute from './routes/indexRoute';
-import subjectRoute from './routes/subjectRoute';
-import lessonRoute from './routes/lessonRoute';
-import noteRoute from './routes/noteRoute';
-import { addRoute, addPOSTRoute } from './routes/addRoutes';
-import exerciseRoute from './routes/exerciseRoute';
-import userRoute  from './routes/userRoute';
-import {loginRoute, loginPOSTRoute} from './routes/loginRoutes';
-import { logoutRoute, logoutPOSTRoute } from './routes/logoutRoutes';
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/static', express.static(`${__dirname}/static`));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cookieParser());
 
 app.use(userAuthData)
+app.use((req, res, next) => {
+  if (req.path.substr(-1) !== '/' && req.path.length > 1) {
+    const query = req.url.slice(req.path.length);
+    res.redirect(301, req.path + '/' + query);
+  } else {
+    next();
+  }
+});
 setupReactViews(app, {
   viewsDirectory: `./tsx-views/`,
   prettify: true, // Prettify HTML output
@@ -74,6 +84,8 @@ app.get('/s/:id/l/:lessonid/n/:noteid', noteRoute)
 app.get('/s/:id/l/:lessonid/add/:type', addRoute)
 app.post('/s/:id/l/:lessonid/add/:type', addPOSTRoute)
 app.get('/s/:id/l/:lessonid/e/:exerciseid', exerciseRoute)
+app.get('/s/:id/l/:lessonid/edit/:type/:id', editRoute)
+app.post('/s/:id/l/:lessonid/edit/:type/:id', editPOSTRoute)
 app.get('/user', userRoute)
 app.get('/user/login', loginRoute)
 app.post('/user/login', loginPOSTRoute)
