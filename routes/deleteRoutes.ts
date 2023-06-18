aa27155dda6d5271403986f8c0c1eea03c9e3069
orl0pl <orlyyt@int.pl>
2023-06-18 16:13:26 +0200

    update

--- routes/deleteRoutes.ts
+++ routes/deleteRoutes.ts
@@ -1,6 +1,29 @@
-import {Request, Response } from 'express';
-export function deleteNoteRoute(req: Request, res: Response) {
-    res.send('delete note')
+import { Request, Response } from 'express';
+import { data, timeAgo } from '../server';
+import iconmapper from '../utils/iconmapper';
+export function deleteNoteRoute(req: Request<{ id: number; lessonid: number; noteid: number; }>, res: Response) {
+    const subject = data.subjects[req.params.id];
+    const lesson = subject.lessons[req.params.lessonid];
+    const note = lesson.notes[req.params.noteid];
+    const isUserAllowed = req.account?.roles.includes('admin');
+    if (lesson && subject && note && isUserAllowed) {
+        res.render('delete', {
+            url: '../'.repeat(7),
+            mi: iconmapper,
+            timeAgo: timeAgo,
+            deletionType: 'note',
+            deletionTypeName: 'notatkę',
+            verificationCode: `${note.id}-${subject.id}-note`,
+            
+        })
+    }
+    else if(!isUserAllowed) {
+        res.send('not allowed');
+    }
+    else {
+        res.send('not found');
+    }
+    
 }
 export function deleteNotePOSTRoute(req: Request, res: Response) {
     res.send('delete note')
