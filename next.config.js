a91303b0ca19b63eec75b86bd3286c00cb73ed06
orl0pl <orlyyt@int.pl>
2023-09-08 16:19:14 +0200

    Fixed require not defined

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
 const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
   i18n,
   webpack: (config) => {
     // this will override the experiments
